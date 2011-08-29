5df4e8ef8d8ad2f07cb32956076623cc46dae93b
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2011-08-28 22:36:59 -0300

    little change in the extension lookup

--- markdown_parser/markdown_parser.py
+++ markdown_parser/markdown_parser.py
@@ -6,6 +6,8 @@ import sys, os
 __author__ = 'Anderson Pierre Cardoso'
 __license__ = 'MIT'
 
+markdown_extensions = ['.markdown', '.md']
+
 def parse_markdown(f, root):
     """
     This function parses a  markdown file.
@@ -18,13 +20,15 @@ def parse_markdown(f, root):
     print 'parsing %s' % (f)
     markdown.markdownFromFile(input=inp, output=out)
 
+def get_extension(f):
+    return f[f.rindex('.'):] if '.' in f else None
 def path_walker(raiz):
     """
     Walks the path tree from a given root folder,
     and parses every file that ends with .markdwon or .md
     """
     for root,dirs,files in os.walk(raiz):
-        [parse_markdown(f, root) for f in files if '.md' in f or '.markdown' in f]
+        [parse_markdown(f, root) for f in files if get_extension(f) in markdown_extensions]
     print('finished path walking =]')
                 
     
