deb14fdb93f502b1c8c68dbf8af2568eeeeeda7e
Anderson Pierre Cardoso <apierre.cardoso@gmail.com>
2011-09-30 21:17:40 -0300

    auto refresh in the browser

--- landslide_presentation/python_brasil7/md_watchdog.py
+++ landslide_presentation/python_brasil7/md_watchdog.py
@@ -3,12 +3,16 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileModifiedEvent
 import sys
 from subprocess import call
+from splinter.browser import Browser
+
+firefox = Browser()
 
 class MdCompileHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if isinstance(event, FileModifiedEvent) and event.src_path.endswith('.md'):
             print 'Change in %s detected... Recompiling markdown' % event.src_path.split('/')[-1]
             call(['landslide', sys.argv[1]])
+            firefox.visit('file:///' + event.src_path.replace('.md', '.html'))
 
 if __name__ == "__main__":
     event_handler = MdCompileHandler()
